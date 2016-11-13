faa9d50010395a78f218aadb9ec0cc6b50816ac3
evan.glicakis <evang9410@gmail.com>
2016-11-13 16:58:49 -0500

    done

--- index.php
+++ index.php
@@ -5,18 +5,24 @@
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="./styles/style.css">
 </head>
-<body>
-    <div class="container col-md-3">
-        <form action="login.php" method="POST">
-          <div class="form-group">
-            <label>Login id:</label>
-            <input type="text" name="login"class="form-control">
-          </div>
-          <div class="form-group">
-            <label for="pwd">Password:</label>
-            <input type="password" name="password" class="form-control" id="pwd">
-          </div>
-          <button type="submit" class="btn btn-default">Login</button>
-        </form>
-        <button type="reg" class="btn btn-primary" onclick="location.href = './signup.html';">Register</button>
-</div>
+<body id="login">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 center-block" id="signup_form">
+                <form action="login.php" method="POST">
+                  <div class="form-group">
+                    <label id="signup_text">Login id:</label>
+                    <input type="text" name="login"class="form-control">
+                  </div>
+                  <div class="form-group">
+                    <label id="signup_text"for="pwd">Password:</label>
+                    <input type="password" name="password" class="form-control" id="pwd">
+                  </div>
+                  <button type="submit" class="btn btn-default">Login</button>
+                </form>
+                <button type="reg" class="btn btn-primary" onclick="location.href = './signup.html';">Register</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
